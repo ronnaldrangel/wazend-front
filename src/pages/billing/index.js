f866eafa5d24961e531f1ed7fc7fd79fc50b90e7
Neopatron Latam <hola@neopatron.com>
2025-02-26 20:03:10 -0500

    .

--- src/pages/billing/index.js
+++ src/pages/billing/index.js
@@ -1,14 +1,32 @@
+import { useState } from 'react';
 import Layout from '../../components/layout/dashboard';
 import Billing from './billing';
 import Subscription from './subscriptions';
 
-
 export default function Index() {
+    const [activeTab, setActiveTab] = useState('subscriptions'); // 'subscriptions' es el valor inicial
+
     return (
         <Layout>
-            <Subscription />
-            <div className='mt-8'>
-            <Billing />
+            <div className="flex space-x-4">
+                {/* Tabs */}
+                <button
+                    onClick={() => setActiveTab('subscriptions')}
+                    className={`px-4 py-2 font-medium rounded-lg ${activeTab === 'subscriptions' ? 'bg-emerald-600 text-white' : 'bg-gray-200'}`}
+                >
+                    Suscripciones
+                </button>
+                <button
+                    onClick={() => setActiveTab('billing')}
+                    className={`px-4 py-2 rounded-lg ${activeTab === 'billing' ? 'bg-emerald-600 text-white' : 'bg-gray-200'}`}
+                >
+                    Historial de pagos
+                </button>
+            </div>
+
+            {/* Content */}
+            <div className="mt-8">
+                {activeTab === 'subscriptions' ? <Subscription /> : <Billing />}
             </div>
         </Layout>
     );
