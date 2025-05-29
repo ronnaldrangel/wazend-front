f168881a37c9f5e5d7f0b8bed721b2b86846e089
Ronald Rangel <ronald@rangel.pro>
2025-05-29 14:28:52 -0500

    listo

--- src/pages/affiliates/index.js
+++ src/pages/affiliates/index.js
@@ -1,11 +1,21 @@
 import Layout from '../../components/layout/dashboard';
 import Affiliates from './affiliates';
+import Request from './request';
 
 export default function Index() {
 
     return (
         <Layout title="Recomienda & Gana">
-            <Affiliates />
+
+            <div className="flex flex-col md:flex-row gap-6">
+                <div className="w-full md:w-2/3">
+                    <Affiliates />
+                </div>
+                <div className="w-full md:w-1/3">
+                    <Request />
+                </div>
+            </div>
+
         </Layout>
     );
 }
