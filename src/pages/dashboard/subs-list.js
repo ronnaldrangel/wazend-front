b2c3aab86132a990e8b615124e7044f1465c5ed2
Ronald <ronald@rangel.pro>
2025-08-27 03:28:58 -0500

    gaa

--- src/pages/dashboard/subs-list.js
+++ src/pages/dashboard/subs-list.js
@@ -1,6 +1,7 @@
+const strapiUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
 const createInstanceUrl = process.env.NEXT_PUBLIC_CREATE_INSTANCE;
+import useSWR from 'swr';
 import { useSession } from 'next-auth/react';
-import { useStrapiData } from '../../services/strapiServiceJWT';
 import InstanceCard from './instance-card';
 import OrderSkeleton from '../../components/loaders/skeleton';
 import Link from 'next/link';
@@ -17,7 +18,24 @@ import { Button } from '@/components/ui/button';
 import { Card } from '@/components/ui/card';
 import Alerta from '@/components/alerts/main';
 
+const fetcher = async (url, jwt) => {
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${jwt}`,
+      },
+    });
 
+    if (!response.ok) {
+      throw new Error(`❌ Error en la solicitud: ${response.statusText}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error('❌ Error al obtener datos de Strapi:', error);
+    throw error;
+  }
+};
 
 const handleCreateInstance = async (subscriptionId, email, mutate, setCreatingInstanceForSubscription) => {
   setCreatingInstanceForSubscription(subscriptionId);
@@ -110,9 +128,9 @@ const FetchStrapi = () => {
   const [showCancelled, setShowCancelled] = useState(false);
   const [creatingInstanceForSubscription, setCreatingInstanceForSubscription] = useState(null);
 
-  const { data, error, isLoading, mutate } = useStrapiData(
-    'users/me?populate[subscriptions][populate]=instances',
-    jwt
+  const { data, error, isLoading, mutate } = useSWR(
+    jwt ? `${strapiUrl}/api/users/me?populate[subscriptions][populate]=instances` : null,
+    (url) => fetcher(url, jwt)
   );
 
   if (isLoading) return <OrderSkeleton />;
